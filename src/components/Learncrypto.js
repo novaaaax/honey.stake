eadb90ae4d06b883b512c92d60e317c066ac58fd
Edwin <edwingenova@gmail.com>
2020-04-05 14:25:26 -0400

    created basic stuctor for app, created routed and placeholder info.

---
+++ src/components/Learncrypto.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Learncrypto(){
+    return(
+        <div>
+            <p>Where out API's and other resources can go.</p>
+        </div>
+    );
+}
