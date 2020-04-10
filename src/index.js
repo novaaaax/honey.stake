b393bead8a994b444fd653734a17a950e01899f6
Long Nguyen <Long.Nguyen@temple.edu>
2020-04-10 12:46:08 -0400

    Home page created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'spectre.css/dist/spectre.min.css';
+import 'spectre.css/dist/spectre-icons.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
