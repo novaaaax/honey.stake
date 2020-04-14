d0e162a154fce56b5715c39cf125e08512b9c8ff
Long Nguyen <Long.Nguyen@temple.edu>
2020-04-14 10:24:22 -0400

    Work in progress learncrypto

---
+++ client/src/components/Learncrypto.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Learncrypto(){
+    return(
+        <div>
+            <p>Where out API's and other resources can go.</p>
+        </div>
+    );
+}
+
+// References for Crypto vidoes and new sites
+// https://cointelegraph.com/
+// https://www.coindesk.com/
+
+// https://medium.com/@stevekrohn/everything-you-need-to-know-about-staking-coins-686ea95041c
+
+// https://trustwallet.com/staking/
+
+// Videos
