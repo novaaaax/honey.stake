f440a466c4c5585adae41c58ccb7dda218087bb2
Edwin <edwingenova@gmail.com>
2020-04-14 15:28:10 -0400

    Made resgistration and sign in

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,10 +7,12 @@ class SignUp extends Component {
         super(props);
 
         this.state = {
+            first_name: "",
+            last_name: "",
             email: "",
             password: "",
-            password_confirmation: "",
-            registrationErrors: ""
+            confirm_password: "",
+            errors: {}
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -26,34 +28,47 @@ class SignUp extends Component {
     handleSubmit(event) {
         console.log('form submitted')
         event.preventDefault();
-        axios.post('https://localhost:3000/signup', {
+        axios.post('/api/createuser', {
+            first_name: this.state.first_name,
+            last_name: this.state.last_name,
             email: this.state.email,
-            password: this.state.password
+            password: this.state.password,
+            confirm_password: this.state.confirm_password
         }).then(response => {
             console.log(response)
             if (response.data) {
                 console.log('signup success')
                 this.setState({
-                    redirectTo: "/login"
+                    redirectTo: "/login",
+                    errors: response.data
                 })
-            } else {
-                console.log('signup error')
             }
         }).catch(err => {
             //this is where i left off
             console.log(err)
+            this.setState({
+                errors: err
+            })
         })
     }
     render() {
         return (
-            <div onSubmit={this.handleSubmit} className="container column col-4 col-xs-12">
+            <div onSubmit={this.handleSubmit} className="container column col-4 col-xs-12"
+                style={{
+                    paddingTop: "100px"
+                }}>
                 <form className="form-group">
+                    <label className="form-label" for="input-example-1">First Name</label>
+                    <input className="form-input" type="text" name="first_name" value={this.state.first_name} id="input-example-1" onChange={this.handleChange} placeholder="First Name" required />
+                    <label className="form-label" for="input-example-1">Last Name</label>
+                    <input className="form-input" type="text" name="last_name" value={this.state.last_name} id="input-example-1" onChange={this.handleChange} placeholder="Last Name" required />
                     <label className="form-label" for="input-example-1">Email</label>
                     <input className="form-input" type="email" name="email" value={this.state.email} id="input-example-1" onChange={this.handleChange} placeholder="Email" required />
                     <label className="form-label" for="input-example-1">Password</label>
                     <input className="form-input" type="password" name="password" value={this.state.password} id="input-example-1" onChange={this.handleChange} placeholder="Password" required />
                     <label className="form-label" for="input-example-1">Confirm Password</label>
-                    <input className="form-input" type="password" name="password_confirmation" value={this.state.password_confirmation} id="input-example-1" onChange={this.handleChange} placeholder="Password Confirmation" required />
+                    <input className="form-input" type="password" name="confirm_password" value={this.state.confirm_password} id="input-example-1" onChange={this.handleChange} placeholder="Confirm Password" required />
+                    {this.state.errors.passwordDoesNotMatch && <small>{this.state.errors.passwordDoesNotMatch}</small>}
                     <button
                         type="submit"
                         style={{
@@ -65,4 +80,4 @@ class SignUp extends Component {
         );
     }
 }
-    export default SignUp
+export default SignUp
