131a74c1e510cd2d9e83813c20201a9e27d8ad10
Edwin <edwingenova@gmail.com>
2020-04-16 14:31:56 -0400

    got logout to work

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,50 +9,46 @@ class Profile extends Component {
             first_name: "",
             errors: {}
         }
-        this.handleSubmit = this.handleSubmit.bind(this)
-
     }
     componentWillMount() {
         this.setState({
             first_name: localStorage.getItem("firstName")
         })
     }
-    handleSubmit() {
-        axios.post("/api/logout", {
+    logout = () => {
+        localStorage.removeItem('jwtToken')
+        localStorage.removeItem('firstName')
+        window.location.replace('/')
+        axios.get("/api/logout", {
 
         })
-            .then(response => {
-                console.log('logged out')
-                console.log(response)
-                localStorage.clear()
-            })
     }
     render() {
         return (
-            
-            <div onSubmit={this.handleSubmit} className="container"style={{
+
+            <div className="container" style={{
                 paddingTop: "60px",
-                textAlign:"center",
-            
-                
+                textAlign: "center",
+
+
             }}>
-               <h1 style={{
-                   fontSize:"40px",
-                   marginTop:"20px"
-               }}
-               >Hello {this.state.first_name}</h1> 
-               <div>
+                <h1 style={{
+                    fontSize: "40px",
+                    marginTop: "20px"
+                }}
+                >Hello {this.state.first_name}</h1>
+                <div>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS3hESwg2C1h208A1VDc1hglIOGz_rWrqtMxcyQHzd5VUBAYhd_&usqp=CAU" alt="profilepic" style={{
-                        borderRadius:"100px",
-                        position:"relative"
-                    }}/>
-                 <h3>Bio:</h3> 
-                 <p className="container"style={{
-                     maxWidth:"400px",
-                     align:"center",
-                 }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt illum quas eos, vel voluptates consequatur sed nemo, exercitationem et unde minus distinctio iure iste maxime eaque officiis accusantium! Deserunt, placeat.</p>
+                        borderRadius: "100px",
+                        position: "relative"
+                    }} />
+                    <h3>Bio:</h3>
+                    <p className="container" style={{
+                        maxWidth: "400px",
+                        align: "center",
+                    }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt illum quas eos, vel voluptates consequatur sed nemo, exercitationem et unde minus distinctio iure iste maxime eaque officiis accusantium! Deserunt, placeat.</p>
                 </div>
-                <button type="submit">Logout</button>
+                <button onClick={this.logout} type="submit">Logout</button>
             </div>
         );
     }
