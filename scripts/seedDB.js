9356409cff1487dc3522b4aecc680c9521716063
Paul Fodrovics <pefodrovics@gmail.com>
2020-04-17 12:54:46 -0400

    deploy

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models');
 mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/solvestakingdb"
+    process.env.MONGODB_URI || "mongodb://Paul:123456A@ds135797.mlab.com:35797/heroku_rhsl61z5"
     );
 
 const exampleUser = [
@@ -10,7 +10,7 @@ const exampleUser = [
         last_name: "Dylan",
         email: "bobdylan@bobdylan.com",
         password: "12345678",
-        date: new Date(Date.now())
+        // date: new Date(Date.now())
     }
 ]
 
